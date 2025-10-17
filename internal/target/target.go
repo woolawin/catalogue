04a9e26a010866de778bbf2f046cad402f266f50
Blair Harris <woolawin@gmail.com>
2025-10-17 06:51:14 +0200

    targets

---
+++ internal/target/target.go
@@ -0,0 +1,116 @@
+package target
+
+import (
+	"fmt"
+	"math"
+	"runtime"
+)
+
+type Architecture string
+
+const (
+	AMD64 Architecture = "amd64"
+	ARM64 Architecture = "arm64"
+)
+
+type Target struct {
+	Name         string
+	Architecture Architecture
+}
+
+type System struct {
+	Architecture Architecture
+}
+
+func GetSystem() (System, error) {
+	system := System{}
+	arch, ok := getArch(runtime.GOARCH)
+	if !ok {
+		return System{}, fmt.Errorf("unknown system architecture '%s'", runtime.GOARCH)
+	}
+	system.Architecture = arch
+	return system, nil
+}
+
+func (system System) Rank(targets []Target) []int {
+	if len(targets) == 0 {
+		return nil
+	}
+	scores := make(map[string]int)
+	for _, target := range targets {
+		score, applicable := target.Score(system)
+		if !applicable {
+			continue
+		}
+		scores[target.Name] = score
+	}
+
+	var ranking []int
+	previous := math.MaxInt32
+	for {
+		if len(ranking) == len(scores) {
+			break
+		}
+		high := -1
+		for _, score := range scores {
+			if score > high && score < previous {
+				high = score
+			}
+		}
+		if high == -1 {
+			break
+		}
+		for target, score := range scores {
+			if score != high {
+				continue
+			}
+		findTarget:
+			for idx := range targets {
+				if targets[idx].Name == target {
+					ranking = append(ranking, idx)
+					break findTarget
+				}
+			}
+		}
+		previous = high
+	}
+	return ranking
+}
+
+func (target Target) Score(system System) (int, bool) {
+	if target.Architecture != system.Architecture {
+		return 0, false
+	}
+
+	return 1, true
+}
+
+func IsReservedTargetName(value string) bool {
+	return value == "all" ||
+		value == "amd64" ||
+		value == "arm64"
+}
+
+func getArch(value string) (Architecture, bool) {
+	switch value {
+	case "amd64":
+		return AMD64, true
+	case "arm64":
+		return ARM64, true
+	default:
+		return "", false
+	}
+}
+
+func BuiltIns() []Target {
+	return []Target{
+		{
+			Name:         "amd64",
+			Architecture: AMD64,
+		},
+		{
+			Name:         "arm",
+			Architecture: ARM64,
+		},
+	}
+}
---
+++ internal/target/target_test.go
@@ -0,0 +1,42 @@
+package target
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestRank(t *testing.T) {
+	t.Run("first", func(t *testing.T) {
+		targets := []Target{
+			{Name: "amd64", Architecture: AMD64},
+			{Name: "arm64", Architecture: ARM64},
+		}
+
+		system := System{Architecture: AMD64}
+
+		actual := system.Rank(targets)
+		expected := []int{0}
+
+		if diff := cmp.Diff(actual, expected); diff != "" {
+			fmt.Printf("Mismatch (-actual +expected):\n%s", diff)
+		}
+	})
+
+	t.Run("second", func(t *testing.T) {
+		targets := []Target{
+			{Name: "amd64", Architecture: AMD64},
+			{Name: "arm64", Architecture: ARM64},
+		}
+
+		system := System{Architecture: ARM64}
+
+		actual := system.Rank(targets)
+		expected := []int{1}
+
+		if diff := cmp.Diff(actual, expected); diff != "" {
+			fmt.Printf("Mismatch (-actual +expected):\n%s", diff)
+		}
+	})
+}
