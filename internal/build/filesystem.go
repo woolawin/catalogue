f51cde09460c47c238b9976a3d162bbc0fe8cef0
Blair Harris <woolawin@gmail.com>
2025-10-17 18:15:39 +0200

    rename disk to FileSystem

---
+++ internal/build/filesystem.go
@@ -0,0 +1,98 @@
+package build
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+
+	"github.com/woolawin/catalogue/internal/target"
+)
+
+func filesystem(src BuildSrc, system target.System) error {
+	fsPath := filePath(src, "filesyste,")
+	exists, asDir, err := dirExists(fsPath)
+	if err != nil {
+		return err
+	}
+	if !asDir {
+		return fmt.Errorf("filesystem is not a directory")
+	}
+
+	if !exists {
+		return nil
+	}
+
+	_, dirs, err := lsDir(fsPath)
+	if err != nil {
+		return err
+	}
+
+	var filesystems []FileSystem
+	for _, dir := range dirs {
+		ref, err := parseFileSystemRef(dir)
+		if err != nil {
+			return err
+		}
+
+		files, err := lsDirFilesRec(filePath(src, "filesystem", dir))
+		if err != nil {
+			return fmt.Errorf("failed to list directory %s: %w", dir, err)
+		}
+
+		var filesystem *FileSystem
+		for _, fs := range filesystems {
+			if fs.Anchor == ref.Anchor {
+				filesystem = &fs
+				break
+			}
+		}
+		if filesystem == nil {
+			filesystems = append(filesystems, FileSystem{Anchor: ref.Anchor})
+			filesystem = &filesystems[len(filesystems)-1]
+		}
+		filesystem.Targets = append(filesystem.Targets, ref.Target)
+		filesystem.TargetFiles[ref.Target] = files
+	}
+
+	return nil
+}
+
+type FileSystem struct {
+	Anchor      string
+	Targets     []string
+	TargetFiles map[string][]string
+}
+
+type FileSystemRef struct {
+	Anchor  string
+	Targets []string
+	Target  string
+}
+
+func parseFileSystemRef(value string) (FileSystemRef, error) {
+	idx := strings.Index(value, ".")
+	if idx == -1 {
+		return FileSystemRef{}, fmt.Errorf("invalid filesystem reference '%s', missing target", value)
+	}
+	anchor := value[:idx]
+	err := validAnchorName(anchor)
+	if err != nil {
+		return FileSystemRef{}, err
+	}
+	targetStr := value[idx+1:]
+	targets, err := target.ParseTargetNamesString(targetStr)
+	if err != nil {
+		return FileSystemRef{}, err
+	}
+	return FileSystemRef{Anchor: anchor, Targets: targets, Target: targetStr}, nil
+}
+
+func validAnchorName(value string) error {
+	for _, r := range value {
+		if unicode.IsLower(r) || string(r) == "_" {
+			continue
+		}
+		return fmt.Errorf("invalid anchor name '%s', '%s' not valid", value, string(r))
+	}
+	return nil
+}
--- internal/build/filesystem_test.go
+++ internal/build/filesystem_test.go
@@ -6,40 +6,40 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestParseDiskRef(t *testing.T) {
+func TestParseFileSystemRef(t *testing.T) {
 	t.Run("invalid_no_targets", func(t *testing.T) {
-		_, err := parseDiskRef("root")
+		_, err := parseFileSystemRef("root")
 		if err == nil {
 			t.Fatal("expetced to FAIL")
 		}
 	})
 
 	t.Run("valid_1_target", func(t *testing.T) {
-		actual, err := parseDiskRef("root.foo")
+		actual, err := parseFileSystemRef("root.foo")
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		expected := DiskRef{Anchor: "root", Targets: []string{"foo"}, Target: "foo"}
+		expected := FileSystemRef{Anchor: "root", Targets: []string{"foo"}, Target: "foo"}
 		if diff := cmp.Diff(actual, expected); diff != "" {
 			t.Fatalf("Mismatch (-actual +expected):\n%s", diff)
 		}
 	})
 
 	t.Run("valid_2_targets", func(t *testing.T) {
-		actual, err := parseDiskRef("root.foo-bar")
+		actual, err := parseFileSystemRef("root.foo-bar")
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		expected := DiskRef{Anchor: "root", Targets: []string{"foo", "bar"}, Target: "foo-bar"}
+		expected := FileSystemRef{Anchor: "root", Targets: []string{"foo", "bar"}, Target: "foo-bar"}
 		if diff := cmp.Diff(actual, expected); diff != "" {
 			t.Fatalf("Mismatch (-actual +expected):\n%s", diff)
 		}
 	})
 
 	t.Run("invalid_anchor", func(t *testing.T) {
-		_, err := parseDiskRef("b2b-ccc")
+		_, err := parseFileSystemRef("b2b-ccc")
 		if err == nil {
 			t.Fatal("expected to FAIL")
 		}
