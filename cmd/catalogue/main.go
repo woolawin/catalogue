7f61f79012c1157ca075d82b29ab407e96d438d7
Blair Harris <woolawin@gmail.com>
2025-10-19 00:59:35 +0200

    add version and system cmd

--- cmd/catalogue/main.go
+++ cmd/catalogue/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/woolawin/catalogue/internal"
@@ -13,12 +15,42 @@ import (
 	"github.com/woolawin/catalogue/internal/target"
 )
 
+//go:embed version.txt
+var Version string
+
 func main() {
 	cmds := args()
 	if err := cmds.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func runVersion(cmd *cobra.Command, args []string) {
+	fmt.Print(strings.TrimSpace(Version))
+}
+
+func runSystem(cmd *cobra.Command, args []string) {
+	system, err := target.GetSystem()
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	builder := strings.Builder{}
+	builder.WriteString("Arhiteture: ")
+	builder.WriteString(string(system.Architecture))
+	builder.WriteString("\nOSReleaseID: ")
+	builder.WriteString(system.OSReleaseID)
+	builder.WriteString("\nOSReleaseVersion: ")
+	builder.WriteString(system.OSReleaseVersion)
+	builder.WriteString("\nOSReleaseVersionID: ")
+	builder.WriteString(system.OSReleaseVersionID)
+	builder.WriteString("\nOSReleaseVersionCodeName: ")
+	builder.WriteString(system.OSReleaseVersionCodeName)
+
+	fmt.Println(builder.String())
 }
 
 func runAdd(cmd *cobra.Command, args []string) {
@@ -28,20 +60,26 @@ func runAdd(cmd *cobra.Command, args []string) {
 func runBuild(cmd *cobra.Command, args []string) {
 	system, err := target.GetSystem()
 	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err.Error())
+		os.Exit(1)
 		return
 	}
 	src, err := cmd.Flags().GetString("src")
 	if err != nil {
 		fmt.Println("BAD COMMAND: --src flag missing")
+		os.Exit(1)
 	}
 	dst, err := cmd.Flags().GetString("dst")
 	if err != nil {
 		fmt.Println("BAD COMMAND: --dst flag missing")
+		os.Exit(1)
 	}
 
 	srcAbs, err := filepath.Abs(src)
 	if err != nil {
 		fmt.Println("BAD COMMAND: source is not a valid path")
+		os.Exit(1)
 	}
 
 	api := ext.NewAPI(srcAbs)
@@ -50,12 +88,14 @@ func runBuild(cmd *cobra.Command, args []string) {
 	if err != nil {
 		fmt.Println("ERROR")
 		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
 	err = build.Build(dst, index, system, api)
 	if err != nil {
 		fmt.Println("ERROR")
 		fmt.Println(err.Error())
+		os.Exit(1)
 	} else {
 		fmt.Println("COMPLETED")
 	}
@@ -64,7 +104,7 @@ func runBuild(cmd *cobra.Command, args []string) {
 func args() *cobra.Command {
 	var add = &cobra.Command{
 		Use:   "add",
-		Short: "Add a package to your system",
+		Short: "Add a package or repository to your local system (does not install)",
 		Long:  ``,
 		Run:   runAdd,
 	}
@@ -78,13 +118,28 @@ func args() *cobra.Command {
 	build.Flags().String("src", "", "Source directory to build from")
 	build.Flags().String("dst", "", "Destination of the package archive")
 
+	var printSystem = &cobra.Command{
+		Use:   "system",
+		Short: "Print system values used for targets",
+		Long:  "",
+		Run:   runSystem,
+	}
+
+	var version = &cobra.Command{
+		Use:   "version",
+		Short: "Print catalogue CLI tool version",
+		Long:  "",
+		Run:   runVersion,
+	}
+
 	var root = &cobra.Command{
 		Use:   "catalogue",
-		Short: "A simple CLI application built with Cobra",
-		Long:  `mycli is a custom command-line tool. It provides features like starting a server with the 'serve' subcommand.`,
+		Short: "The missing piece to APT. An APT Repository Middleware",
 	}
 
 	root.AddCommand(add)
 	root.AddCommand(build)
+	root.AddCommand(printSystem)
+	root.AddCommand(version)
 	return root
 }
