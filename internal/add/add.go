954954f1ed6afd61415bfe1f708f4a33fc35a035
Blair Harris <woolawin@gmail.com>
2025-10-19 16:49:51 +0200

    write config file

--- internal/add/add.go
+++ internal/add/add.go
@@ -1,6 +1,8 @@
 package add
 
 import (
+	"bytes"
+
 	"github.com/woolawin/catalogue/internal"
 	"github.com/woolawin/catalogue/internal/build"
 	"github.com/woolawin/catalogue/internal/clone"
@@ -33,5 +35,32 @@ func Add(protocol clone.Protocol, remote string, system target.System, api ext.A
 		return internal.Err("component '%s' has no supported target")
 	}
 
+	if config.Kind == component.Package {
+		return addPackage(config, api)
+	}
+
+	return internal.Err("only packages can be added right now")
+}
+
+func addPackage(config component.Config, api ext.API) error {
+	exists, err := api.Host().HasPackage(config.Name)
+	if err != nil {
+		return internal.ErrOf(err, "failed tocheck if package '%s' already exists", config.Name)
+	}
+	if exists {
+		return internal.Err("package '%s' alreadt exists", config.Name)
+	}
+
+	disk := api.Host().PackageDisk(config.Name)
+
+	var buffer bytes.Buffer
+	err = component.Serialize(config, &buffer)
+	if err != nil {
+		return internal.ErrOf(err, "can not serialize config")
+	}
+	err = disk.WriteFile("config.toml", &buffer)
+	if err != nil {
+		return internal.ErrOf(err, "can not write package config")
+	}
 	return nil
 }
---
+++ internal/component/serialize.go
@@ -0,0 +1,75 @@
+package component
+
+import (
+	"io"
+
+	toml "github.com/pelletier/go-toml/v2"
+	"github.com/woolawin/catalogue/internal"
+)
+
+func Serialize(config Config, writer io.Writer) error {
+	tml := ConfigTOML{}
+	tml.Name = config.Name
+
+	switch config.Kind {
+	case Package:
+		tml.Kind = "package"
+	case Repository:
+		tml.Kind = "repository"
+	}
+
+	for _, supported := range config.SupportsTargets {
+		tml.SupportsTargets = append(tml.SupportsTargets, supported.Name)
+	}
+
+	for _, tgt := range config.Targets {
+		if tml.Target == nil {
+			tml.Target = make(map[string]TargetTOML)
+		}
+		tml.Target[tgt.Name] = TargetTOML{
+			Architecture:             string(tgt.Architecture),
+			OSReleaseID:              tgt.OSReleaseID,
+			OSReleaseVersion:         tgt.OSReleaseVersion,
+			OSReleaseVersionID:       tgt.OSReleaseVersionID,
+			OSReleaseVersionCodeName: tgt.OSReleaseVersionCodeName,
+		}
+	}
+
+	for _, metadata := range config.Metadata {
+		if tml.Metadata == nil {
+			tml.Metadata = make(map[string]MetadataTOML)
+		}
+		tml.Metadata[metadata.Target.Name] = MetadataTOML{
+			Dependencies:    metadata.Dependencies,
+			Section:         metadata.Section,
+			Priority:        metadata.Priority,
+			Homepage:        metadata.Homepage,
+			Maintainer:      metadata.Maintainer,
+			Description:     metadata.Description,
+			Architecture:    metadata.Architecture,
+			Recommendations: metadata.Recommendations,
+		}
+	}
+
+	for name, targets := range config.Downloads {
+		if tml.Download == nil {
+			tml.Download = make(map[string]map[string]DownloadTOML)
+		}
+		for _, download := range targets {
+			_, ok := tml.Download[name]
+			if !ok {
+				tml.Download[name] = make(map[string]DownloadTOML)
+			}
+			tml.Download[name][download.Target.Name] = DownloadTOML{
+				Source:      download.Source.String(),
+				Destination: download.Destination.String(),
+			}
+		}
+	}
+
+	err := toml.NewEncoder(writer).Encode(&tml)
+	if err != nil {
+		return internal.ErrOf(err, "failed to serialize component config")
+	}
+	return nil
+}
