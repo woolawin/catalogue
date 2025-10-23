792311e352d41137eae37ae01da41525ea103b0b
Blair Harris <woolawin@gmail.com>
2025-10-23 18:03:44 +0200

    call clone

--- internal/assmeble/assemble.go
+++ internal/assmeble/assemble.go
@@ -4,14 +4,15 @@ import (
 	"io"
 
 	"github.com/woolawin/catalogue/internal"
+	"github.com/woolawin/catalogue/internal/clone"
 	"github.com/woolawin/catalogue/internal/config"
 	"github.com/woolawin/catalogue/internal/ext"
 	reg "github.com/woolawin/catalogue/internal/registry"
 )
 
-func Assemble(log *internal.Log, dst io.Writer, component config.Component, api ext.API, registry reg.Registry) bool {
+func Assemble(log *internal.Log, dst io.Writer, component config.Component, api *ext.API, registry reg.Registry) bool {
 	log.Msg(10, "Assembling package").With("name", component.Name).Info()
-	_, found, err := registry.GetPackageRecord(component.Name)
+	record, found, err := registry.GetPackageRecord(component.Name)
 	if err != nil {
 		log.Msg(10, "Failed to get package record").Error()
 		return false
@@ -21,6 +22,9 @@ func Assemble(log *internal.Log, dst io.Writer, component config.Component, api 
 		log.Msg(10, "Could not find package record").Error()
 		return false
 	}
+
+	local := api.Host.RandomTmpDir()
+	clone.Clone(record.Origin.Type, record.Origin.URL.String(), local, "", api)
 
 	return true
 }
