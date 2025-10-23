a20d13329844212444fd4971cdbf64cacb4526e7
Blair Harris <woolawin@gmail.com>
2025-10-23 18:06:05 +0200

    call build

--- internal/assmeble/assemble.go
+++ internal/assmeble/assemble.go
@@ -4,13 +4,14 @@ import (
 	"io"
 
 	"github.com/woolawin/catalogue/internal"
+	"github.com/woolawin/catalogue/internal/build"
 	"github.com/woolawin/catalogue/internal/clone"
 	"github.com/woolawin/catalogue/internal/config"
 	"github.com/woolawin/catalogue/internal/ext"
 	reg "github.com/woolawin/catalogue/internal/registry"
 )
 
-func Assemble(log *internal.Log, dst io.Writer, component config.Component, api *ext.API, registry reg.Registry) bool {
+func Assemble(log *internal.Log, dst io.Writer, component config.Component, system internal.System, api *ext.API, registry reg.Registry) bool {
 	log.Msg(10, "Assembling package").With("name", component.Name).Info()
 	record, found, err := registry.GetPackageRecord(component.Name)
 	if err != nil {
@@ -24,7 +25,15 @@ func Assemble(log *internal.Log, dst io.Writer, component config.Component, api 
 	}
 
 	local := api.Host.RandomTmpDir()
-	clone.Clone(record.Origin.Type, record.Origin.URL.String(), local, "", api)
-
+	err = clone.Clone(record.Origin.Type, record.Origin.URL.String(), local, "", api)
+	if err != nil {
+		log.Msg(10, "failed to clone package source").Error()
+		return false
+	}
+	err = build.Build(dst, component, system, ext.NewAPI(local))
+	if err != nil {
+		log.Msg(10, "failed to build package").Error()
+		return false
+	}
 	return true
 }
