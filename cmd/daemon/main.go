30998744c5fc84acaef6914c4dedfd95bb8401bb
Blair Harris <woolawin@gmail.com>
2025-10-23 11:52:09 +0200

    separate different types of server

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -8,26 +8,30 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/woolawin/catalogue/internal/daemon"
+	"github.com/woolawin/catalogue/internal/ext"
 	reg "github.com/woolawin/catalogue/internal/registry"
 )
 
 func main() {
-	registry := reg.NewRegistry()
-	server := NewHTTPServer(registry)
 
-	err := server.start()
+	registry := reg.NewRegistry()
+	api := ext.NewAPI("/")
+	system, err := api.Host.GetSystem()
 	if err != nil {
 		fmt.Println("ERROR")
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
+	server := daemon.NewServer(system, api, registry)
+
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 	<-shutdown
 
-	ctx, release := context.WithTimeout(context.Background(), 10*time.Second)
+	_, release := context.WithTimeout(context.Background(), 10*time.Second)
 	defer release()
 
-	server.Shutdown(ctx)
+	server.Shutdown()
 }
